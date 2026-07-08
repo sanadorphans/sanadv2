fb0f5ad837e9abca7d154e0dc1bf0f8348c4bcaa
Ahmed Essam <mc@sanadorphans.org>
2026-07-08 10:27:08 +0300

    Update KnowledgeController.php

--- app/Http/Controllers/cms/KnowledgeController.php
+++ app/Http/Controllers/cms/KnowledgeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\cms;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\knowledge;
 
 class KnowledgeController extends Controller
 {
